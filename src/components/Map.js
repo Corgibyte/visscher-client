39d9ba355d6d1e2da9d51dfa451d99ece20f106b
Hannah Young <hannah@corgibyte.com>
2022-03-08 11:06:59 -0800

    Implement data from API rendering on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react';
+import { Vector as VectorSource } from 'ol/source';
+import { Vector as VectorLayer } from 'ol/layer';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Fill, Style, Text, Stroke, Circle } from 'ol/style';
+import { fromLonLat } from 'ol/proj';
 import * as ol from 'ol';
 import XYZ from 'ol/source/XYZ';
 import TileLayer from 'ol/layer/Tile';
@@ -10,32 +16,91 @@ const mapStyle = {
   height: "500px"
 };
 
+const textStyle = (feature) => {
+  return new Style({
+    image: new Circle({
+      radius: 10,
+      fill: new Fill({ color: 'rgba(255, 0, 0, 0.1)' }),
+      stroke: new Stroke({ color: 'red', width: 1 })
+    }),
+    text: new Text({
+      font: 'bold 11px "Open Sans", "Arial Unicode MS", "sans-serif"',
+      placement: 'point',
+      fill: new Fill({
+        color: 'black',
+      }),
+      text: feature.get('name')
+    }),
+  });
+}
+
 function Map() {
   const mapRef = useRef();
   const [map, setMap] = useState(null);
+  const [isLoading, toggleLoading] = useState(true);
+  const [error, toggleError] = useState(false);
 
   useEffect(() => {
-    let options = {
-      view: new ol.View({
-        center: [0, 0],
-        zoom: 2
-      }),
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    // API Helper
+    const runFetch = async () => {
+      fetch('http://localhost:5000/api/Battles?startYear=9999&endYear=9999')
+        .then(response => response.json())
+        .then(
+          (jsonifiedResponse) => {
+            createMap(jsonifiedResponse);
+            toggleLoading(false);
           })
-        })
-      ]
-    };
-    let mapObject = new ol.Map(options);
-    mapObject.setTarget(mapRef.current);
-    setMap(mapObject);
-    return () => mapObject.setTarget(undefined);
+        .catch((error) => {
+          toggleError(true);
+        });
+    }
+    const createMap = (jsonData) => {
+      var features = jsonData.map(x => new Feature({
+        geometry: new Point(fromLonLat(x.coordinates)),
+        name: x.Name
+      }));
+      const vectorSource = new VectorSource();
+      vectorSource.addFeatures(features);
+      const vectorLayer = new VectorLayer({
+        source: vectorSource,
+        style: textStyle,
+        declutter: true
+      });
+
+      let options = {
+        view: new ol.View({
+          center: [0, 0],
+          zoom: 2
+        }),
+        layers: [
+          new TileLayer({
+            source: new XYZ({
+              url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+          }),
+          vectorLayer
+        ]
+      };
+      let mapObject = new ol.Map(options);
+      mapObject.setTarget(mapRef.current);
+      setMap(mapObject);
+    }
+    runFetch();
+    return () => map.setTarget(undefined);
   }, []);
 
+  const chooseRender = () => {
+    if (isLoading) {
+      return <p>Loading...</p>;
+    } else if (error) {
+      return <p>Error!</p>
+    }
+  }
+
   return (
-    <div ref={mapRef} style={mapStyle}></div>
+    <div ref={mapRef} style={mapStyle}>
+      {chooseRender()}
+    </div>
   );
 }
 
